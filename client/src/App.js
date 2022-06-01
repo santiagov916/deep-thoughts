dd297cddb9eac8ded2c132bce60aacf488f7491f
Santiago Venegas <santiagovenegas@Santiagos-MacBook-Pro.local>
2022-06-01 03:07:09 -0700

    react ready for front to back end communication

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,33 @@
 import React from 'react';
+import { ApolloProvider, ApolloClient, InMemeryCache, createHttpLink } from '@apollo/client';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 import Home from './pages/Home';
+import { create } from 'lodash';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3001/graphql',
+
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemeryCache(),
+});
 
 function App() {
   return (
-    <div className='flex-column justify-flex-start min-100-vh'>
-      <Header />
-      <div className='container'>
-        <Home />
+    <ApolloProvider>
+      <div className='flex-column justify-flex-start min-100-vh'>
+        <Header />
+        <div className='container'>
+          <Home />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </ApolloProvider>
   );
 }
 
