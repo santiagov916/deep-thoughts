b090dadae5c1d41dcc16677ea86f430c734b31b7
Santiago Venegas <santiagovenegas@Santiagos-MacBook-Pro.local>
2022-08-26 17:44:01 -0700

    routes for manually typing in navigation are good, links coming up

--- deep-thoughts/client/src/App.js
+++ deep-thoughts/client/src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -19,13 +26,42 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
+      <Router>
+
       <div className='flex-column justify-flex-start min-100-vh'>
         <Header />
         <div className='container'>
-          <Home />
+          <Routes>
+            <Route
+            path="/"
+            element={<Home />}
+            />
+            <Route
+            path="/login"
+            element={<Login />}
+            />
+            <Route
+            path="/signup"
+            element={<Signup />}
+            />
+            <Route
+            path="/profile"
+            element={<Profile />}
+            />
+            <Route
+            path="/thought"
+            element={<SingleThought />}
+            />
+          <Route 
+          path="*"
+          element={<NoMatch />}
+          />
+          </Routes>
+
         </div>
         <Footer />
       </div>
+      </Router>
     </ApolloProvider>
   );
 }
