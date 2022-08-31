abb7d45647ce796ecc1d542d3b9bc1dfdf055dd0
Santiago Venegas <santiagovenegas@Santiagos-MacBook-Pro.local>
2022-08-31 00:04:56 -0700

    reactions are now available!

---
+++ deep-thoughts/client/src/components/ReactionForm/index.js
@@ -0,0 +1,56 @@
+import { useMutation } from '@apollo/client';
+import React, { useState } from 'react';
+
+import { ADD_REACTION } from '../../utils/mutations';
+
+const ReactionForm = ({ thoughtId }) => {
+
+    const [addReaction, { error }] = useMutation(ADD_REACTION);
+
+    const [reactionBody, setBody] = useState('');
+
+    const [characterCount, setCharacterCount] = useState(0);
+
+    const handleChange = event => {
+        if (event.target.value.length <= 280) {
+            setBody(event.target.value);
+            setCharacterCount(event.target.value.length);
+        }
+    };
+
+    const handleFormSubmit = async event => {
+        event.preventDefault();
+
+        await addReaction({
+            variables: { thoughtId, reactionBody }
+        })
+
+        setBody('');
+        setCharacterCount(0);
+    }
+
+    return (
+        <div>
+            <p className='m-0'>
+                characterCount: {characterCount}/280
+                {error && <span className='ml-2'>Something went wrong...</span>}
+            </p>
+            <form 
+            className='flex-row justify-center justify-space-between-md align-stretch'
+            onClick={handleFormSubmit}>
+                <textarea
+                value={reactionBody}
+                placeholder='Leave a reaction to this thought...'
+                className='form-input col-12 col-md-9'
+                onChange={handleChange}
+                ></textarea>
+
+                <button className='btn col-12 col-md-3' type='submit'>
+                    Submit
+                </button>
+            </form>
+        </div>
+    );
+};
+
+export default ReactionForm;
